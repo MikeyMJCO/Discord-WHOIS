78c1cba7f5a2c9f4ba7a33133f33cd66a3209b9b
BadBoyHaloCat <nutdriver716@gmail.com>
2020-08-25 09:46:12 -0700

    a

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const discord = require("discord.js"), // Discord.JS is the primary library used
 const config = require("./config.json"),
     package = require("./package.json")
 
-// Functions
+// Log function
 function log(type = 'info', log = 'No log text specified') {
     if (type === 'info') {
         console.log(chalk.blue(chalk.bold('[INFO]') + ' ' + log))
@@ -32,16 +32,39 @@ function log(type = 'info', log = 'No log text specified') {
 log('info', 'Discord WHOIS')
 log('info', 'Version ' + package.version)
 log('info', 'Developed by BadBoyHaloCat')
+log('info', 'Launching client...')
 
 // Launch client instance
 const client = new discord.Client() // The client is the bot itself. We create it here.
 
+log('info', 'Loading databases...')
 // Databases
 const db = new enmap({ name: 'db' }) // This is the primary database.
 
+log('info', 'Loading commands...')
 // Command Handler
 client.commands = new enmap() // Not going to be persistant. It's the command loader.
 
 fs.readdir('./commands/', (err, files) => {
-
+    if (err) {
+        log('error', 'Failed to read ./commands/. Does it exist? ' + err)
+        process.exit()
+    } else {
+        files.forEach((file) => {
+            if (file.endsWith('.js')) {
+                let commandname = file.split('.').slice(0, file.split('.').length - 1)
+                log('info', 'Loading command ' + commandname)
+                fs.readFile('./commands/' + file, 'utf8', (err, contents) => {
+                    if (err) {
+                        log('error', 'Failed to load command ' + commandname + ', skipping.')
+                    } else {
+                        log('info', 'Loading command ' + commandname)
+                        fs.readFile
+                    }
+                })
+            } else {
+                log('warning', 'Found file ' + file + ' in the commands directory. This is not going to be loaded as it is not a .js file.')
+            }
+        })
+    }
 })
