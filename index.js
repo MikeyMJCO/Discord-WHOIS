d026826d2f865c86e913b513859e617dce779f0d
BadBoyHaloCat <nutdriver716@gmail.com>
2020-08-25 08:45:07 -0700

    Initial code

--- index.js
+++ index.js
@@ -7,3 +7,12 @@
  * 
  * GitHub: https://github.com/thetayloredman/discord-whois
  */
+
+// Load packages
+const discord = require("discord.js"), // Discord.JS is the primary library used to run the bot.
+    enmap = require("enmap") // Enmap is the database
+
+// Launch client instance
+const client = new discord.Client() // The client is the bot itself. We create it here.
+
+// Databases
